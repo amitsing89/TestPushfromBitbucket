91c278bf455c09a8277f4b156ddc7bcc6a1879d4
amitsing89 <coolrohan.92@gmail.com>
2020-10-28 23:04:41 +0530

    Removed Unwanted things

--- DataStructures/python/check.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-python /root/check/IdeaProjects/pythonscripts/DataStructures/learning/datastructures/python/TimeCheck.py 4 >> runtime.text &
-python /root/check/IdeaProjects/pythonscripts/DataStructures/learning/datastructures/python/TimeCheck.py 4 >> runtime.text &
-python /root/check/IdeaProjects/pythonscripts/DataStructures/learning/datastructures/python/TimeCheck.py 4 >> runtime.text &
-python /root/check/IdeaProjects/pythonscripts/DataStructures/learning/datastructures/python/TimeCheck.py 4 >> runtime.text &
